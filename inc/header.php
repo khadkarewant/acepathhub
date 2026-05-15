2966e30569fcf280d5e28760e71af97a5152abd4
khadkarewant <khadkarewant47@gmail.com>
2026-05-15 21:52:35 +0545

    feat: replace sidebar images with Font Awesome icons, add FA CDN

--- inc/header.php
+++ inc/header.php
@@ -31,16 +31,16 @@ $user_id = $_SESSION['user_id'] ?? null;
                     <img src="https://cdn-icons-png.flaticon.com/512/1077/1077114.png" alt="" style="width:30px;">
                     <div class="submenu profile_submenu">
                         <div class="sublinks">
-                            <a href="profile.php"><img src="assets/img/my_profile.png" alt=""> My Profile</a>
+                            <a href="profile.php"><i class="fa-solid fa-user"></i> My Profile</a>
                         </div>
                         <div class="sublinks">
-                            <a href="logout.php"><img src="assets/img/login_out.png" alt=""> Logout</a>
+                            <a href="logout.php"><i class="fa-solid fa-right-from-bracket"></i> Logout</a>
                         </div>
                     </div>
                 </div>
             <?php else: ?>
                 <div class="links">
-                    <a href="login.php"><img src="assets/img/login_out.png" alt=""> Login</a>
+                    <a href="login.php"><i class="fa-solid fa-right-to-bracket"></i> Login</a>
                 </div>
             <?php endif; ?>
         </div>
@@ -55,7 +55,7 @@ $user_id = $_SESSION['user_id'] ?? null;
                 <!-- Dashboard -->
                 <div class="side-links">
                     <a href="<?= isset($_SESSION['user_id']) ? 'home.php' : 'index.php' ?>">
-                        <img src="assets/img/dashboard.png" alt=""> Dashboard
+                        <i class="fa-solid fa-gauge"></i> Dashboard
                     </a>
                 </div>
 
@@ -64,73 +64,73 @@ $user_id = $_SESSION['user_id'] ?? null;
 
                     <div class="side-links">
                         <a href="javascript:void(0)" class="menu-toggle">
-                            <img src="assets/img/user.png" alt=""> Users &triangledown;
+                            <i class="fa-solid fa-users"></i> Users &triangledown;
                         </a>
                         <div class="side-submenu">
                             <div class="side-sublinks">
-                                <a href="users.php"><img src="assets/img/user.png" alt=""> User List</a>
+                                <a href="users.php"><i class="fa-solid fa-users"></i> User List</a>
                             </div>
                             <div class="side-sublinks">
-                                <a href="add-user.php"><img src="assets/img/add_user.png" alt=""> Add User</a>
+                                <a href="add-user.php"><i class="fa-solid fa-user-plus"></i> Add User</a>
                             </div>
                         </div>
                     </div>
 
                     <div class="side-links">
                         <a href="javascript:void(0)" class="menu-toggle">
-                            <img src="assets/img/products.png" alt=""> Products &triangledown;
+                            <i class="fa-solid fa-box"></i> Products &triangledown;
                         </a>
                         <div class="side-submenu">
                             <div class="side-sublinks">
-                                <a href="products.php"><img src="assets/img/products.png" alt=""> Product List</a>
+                                <a href="products.php"><i class="fa-solid fa-box"></i> Product List</a>
                             </div>
                             <div class="side-sublinks">
-                                <a href="add-product.php"><img src="assets/img/add.png" alt=""> Add Product</a>
+                                <a href="add-product.php"><i class="fa-solid fa-plus"></i> Add Product</a>
                             </div>
                             <div class="side-sublinks">
-                                <a href="assign-product.php"><img src="assets/img/add.png" alt=""> Assign Product</a>
+                                <a href="assign-product.php"><i class="fa-solid fa-hand-holding-box"></i> Assign Product</a>
                             </div>
                         </div>
                     </div>
 
                     <div class="side-links">
                         <a href="javascript:void(0)" class="menu-toggle">
-                            <img src="assets/img/data.png" alt=""> MCQ Pool &triangledown;
+                            <i class="fa-solid fa-layer-group"></i> MCQ Pool &triangledown;
                         </a>
                         <div class="side-submenu">
                             <div class="side-sublinks">
-                                <a href="draft-mcqs.php"><img src="assets/img/draft.png" alt=""> Drafted MCQs</a>
+                                <a href="draft-mcqs.php"><i class="fa-solid fa-file-pen"></i> Drafted MCQs</a>
                             </div>
                             <div class="side-sublinks">
-                                <a href="updatable-mcqs.php"><img src="assets/img/updatable.png" alt=""> Updatable MCQs</a>
+                                <a href="updatable-mcqs.php"><i class="fa-solid fa-rotate"></i> Updatable MCQs</a>
                             </div>
                             <div class="side-sublinks">
-                                <a href="unverified-mcqs.php"><img src="assets/img/unverified_data.png" alt=""> Unverified MCQs</a>
+                                <a href="unverified-mcqs.php"><i class="fa-solid fa-circle-exclamation"></i> Unverified MCQs</a>
                             </div>
                             <div class="side-sublinks">
-                                <a href="admin-mcq-reports.php"><img src="assets/img/report.png" alt=""> Reports</a>
+                                <a href="admin-mcq-reports.php"><i class="fa-solid fa-flag"></i> Reports</a>
                             </div>
                         </div>
                     </div>
 
                     <div class="side-links">
-                        <a href="user-stats.php"><img src="assets/img/chart.png" alt=""> Exam Stats</a>
+                        <a href="user-stats.php"><i class="fa-solid fa-chart-bar"></i> Exam Stats</a>
                     </div>
 
                     <div class="side-links">
-                        <a href="purchase-stats.php"><img src="assets/img/chart.png" alt=""> Purchase Stats</a>
+                        <a href="purchase-stats.php"><i class="fa-solid fa-chart-line"></i> Purchase Stats</a>
                     </div>
 
                     <div class="side-links">
                         <a href="javascript:void(0)" class="menu-toggle">
-                            <img src="assets/img/settings.png" alt=""> Settings &triangledown;
+                            <i class="fa-solid fa-gear"></i> Settings &triangledown;
                         </a>
                         <div class="side-submenu">
                             <div class="side-sublinks">
-                                <a href="block-list.php"><img src="assets/img/settings.png" alt=""> Block List</a>
+                                <a href="block-list.php"><i class="fa-solid fa-ban"></i> Block List</a>
                             </div>
                             <div class="side-sublinks">
-                                <a href="admin-chat-reports.php"><img src="assets/img/settings.png" alt=""> Chat Reports</a>
+                                <a href="admin-chat-reports.php"><i class="fa-solid fa-comments"></i> Chat Reports</a>
                             </div>
                         </div>
                     </div>
@@ -141,10 +141,10 @@ $user_id = $_SESSION['user_id'] ?? null;
                 <?php if(has_role(ROLE_DATA_ENTRY)): ?>
 
                     <div class="side-links">
-                        <a href="add-mcq.php"><img src="assets/img/add.png" alt=""> Add MCQ</a>
+                        <a href="add-mcq.php"><i class="fa-solid fa-plus"></i> Add MCQ</a>
                     </div>
                     <div class="side-links">
-                        <a href="unverified-mcqs.php"><img src="assets/img/unverified_data.png" alt=""> Unverified MCQs</a>
+                        <a href="unverified-mcqs.php"><i class="fa-solid fa-circle-exclamation"></i> Unverified MCQs</a>
                     </div>
 
                 <?php endif; ?>
@@ -153,22 +153,22 @@ $user_id = $_SESSION['user_id'] ?? null;
                 <?php if(has_role(ROLE_STUDENT)): ?>
 
                     <div class="side-links">
-                        <a href="products.php"><img src="assets/img/products.png" alt=""> Products</a>
+                        <a href="products.php"><i class="fa-solid fa-book"></i> Products</a>
                     </div>
                     <div class="side-links">
-                        <a href="my-products.php"><img src="assets/img/my_products.png" alt=""> My Products</a>
+                        <a href="my-products.php"><i class="fa-solid fa-star"></i> My Products</a>
                     </div>
                     <div class="side-links">
-                        <a href="course.php"><img src="assets/img/course.png" alt=""> Full Course</a>
+                        <a href="course.php"><i class="fa-solid fa-graduation-cap"></i> Full Course</a>
                     </div>
                     <div class="side-links">
-                        <a href="downloads.php"><img src="assets/img/products.png" alt=""> Downloads</a>
+                        <a href="downloads.php"><i class="fa-solid fa-download"></i> Downloads</a>
                     </div>
                     <div class="side-links">
-                        <a href="leaderboard.php"><img src="assets/img/chart.png" alt=""> Leaderboard</a>
+                        <a href="leaderboard.php"><i class="fa-solid fa-trophy"></i> Leaderboard</a>
                     </div>
                     <div class="side-links">
-                        <a href="user-stats.php"><img src="assets/img/chart.png" alt=""> My Stats</a>
+                        <a href="user-stats.php"><i class="fa-solid fa-chart-simple"></i> My Stats</a>
                     </div>
 
                 <?php endif; ?>
@@ -176,23 +176,23 @@ $user_id = $_SESSION['user_id'] ?? null;
                 <!-- COMMON — logged in -->
                 <?php if(isset($_SESSION['user_id'])): ?>
                     <div class="side-links">
-                        <a href="profile.php"><img src="assets/img/my_profile.png" alt=""> My Profile</a>
+                        <a href="profile.php"><i class="fa-solid fa-user"></i> My Profile</a>
                     </div>
                     <div class="side-links">
-                        <a href="logout.php"><img src="assets/img/login_out.png" alt=""> Logout</a>
+                        <a href="logout.php"><i class="fa-solid fa-right-from-bracket"></i> Logout</a>
                     </div>
                 <?php else: ?>
                     <div class="side-links">
-                        <a href="login.php"><img src="assets/img/login_out.png" alt=""> Login</a>
+                        <a href="login.php"><i class="fa-solid fa-right-to-bracket"></i> Login</a>
                     </div>
                     <div class="side-links">
-                        <a href="signup.php"><img src="assets/img/my_profile.png" alt=""> Register</a>
+                        <a href="signup.php"><i class="fa-solid fa-user-pen"></i> Register</a>
                     </div>
                     <div class="side-links">
-                        <a href="contact.php"><img src="assets/img/course.png" alt=""> Contact Us</a>
+                        <a href="contact.php"><i class="fa-solid fa-phone"></i> Contact Us</a>
                     </div>
                     <div class="side-links">
-                        <a href="about.php"><img src="assets/img/course.png" alt=""> About Us</a>
+                        <a href="about.php"><i class="fa-solid fa-circle-info"></i> About Us</a>
                     </div>
                 <?php endif; ?>
 
--- inc/links.php
+++ inc/links.php
@@ -7,6 +7,7 @@ $page_image       = $page_image       ?? "https://acepathhub.com/assets/img/logo
 
 <!-- CSS CDN -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
 
 <!-- Custom CSS -->
