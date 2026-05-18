b19c1a380ca5a29073bfc1b67a8e04c98524341d
khadkarewant <khadkarewant47@gmail.com>
2026-05-18 14:57:22 +0545

    fix: dataTables bootstrap5 theme and add btn-qs button styles

--- inc/links.php
+++ inc/links.php
@@ -8,8 +8,7 @@ $page_image       = $page_image       ?? "https://acepathhub.com/assets/img/logo
 <!-- CSS CDN -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
-
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
 <!-- Custom CSS -->
 <link rel="stylesheet" href="assets/css/style.css">
 
@@ -33,7 +32,7 @@ $page_image       = $page_image       ?? "https://acepathhub.com/assets/img/logo
 <!-- JS CDN -->
 <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
 
 <!-- Custom JS -->
 <script src="assets/js/app.js"></script>
