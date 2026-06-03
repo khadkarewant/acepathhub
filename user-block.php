8f78b04610269aaba154f8e6dcce32a21113da09
khadkarewant <khadkarewant47@gmail.com>
2026-06-03 14:41:33 +0545

    fix bind_param constant reference in user-block.php

--- user-block.php
+++ user-block.php
@@ -20,9 +20,10 @@ if (!$target_user_id || !in_array($action, ['block', 'unblock'], true)) {
 }
 
 // Confirm user exists and is not admin
+$admin_role = ROLE_ADMIN;
 $stmt = mysqli_prepare($conn,
     'SELECT user_id FROM users WHERE user_id = ? AND role != ? LIMIT 1');
-mysqli_stmt_bind_param($stmt, 'ii', $target_user_id, ROLE_ADMIN);
+mysqli_stmt_bind_param($stmt, 'ii', $target_user_id, $admin_role);
 mysqli_stmt_execute($stmt);
 $res  = mysqli_stmt_get_result($stmt);
 $user = mysqli_fetch_assoc($res);
